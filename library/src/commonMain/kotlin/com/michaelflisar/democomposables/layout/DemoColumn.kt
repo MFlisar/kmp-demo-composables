7e4b55d363695b01d2c6becae7c867efe480ae5f
MFlisar <mflisar.development@gmail.com>
2025-08-20 14:32:24 +0200

    added and changed: DemoRow and DemoColumn

---
+++ library/src/commonMain/kotlin/com/michaelflisar/democomposables/layout/DemoColumn.kt
@@ -0,0 +1,23 @@
+package com.michaelflisar.democomposables.layout
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DemoColumn(
+    modifier: Modifier = Modifier,
+    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = horizontalAlignment,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        content = content
+    )
+}
--- library/src/commonMain/kotlin/com/michaelflisar/democomposables/layout/DemoRow.kt
+++ library/src/commonMain/kotlin/com/michaelflisar/democomposables/layout/DemoRow.kt
@@ -4,11 +4,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun DemoRow(content: @Composable RowScope.() -> Unit) {
+fun DemoRow(
+    modifier: Modifier = Modifier,
+    verticalAlignment: Alignment.Vertical = Alignment.Top,
+    content: @Composable RowScope.() -> Unit
+) {
     Row(
+        modifier = modifier,
+        verticalAlignment = verticalAlignment,
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         content()
